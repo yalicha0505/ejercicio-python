9482d5ca0804d390284134a5b11f1c8fa0a93450
Yady Chamorro <yalicha0505@gmail.com>
2024-10-10 15:01:56 -0500

    Ajustes ejercicios entradas y salidas

--- entradas-salidas/preguntar_nombre.py
+++ entradas-salidas/preguntar_nombre.py
@@ -1,11 +1,11 @@
 # solicitar al usuario su nombre
-nombre = input("¿Cuál es tu nombre?:")
+nombre = input("¿Cuál es tu nombre?: ")
 
 # solictar al usuario su edad 
-edad = input("¿Cuál es tu edad?:")
+edad = input("¿Cuál es tu edad?: ")
 
 # solicitar al usuario su sexo
-sexo = input("¿Cuál es tu sexo?:")
+sexo = input("¿Cuál es tu sexo?: ")
 
 # mostrar resultado
-print(f"Te llamas: {nombre}, tu edad: {edad}, eres: {sexo}")
+print(f"Te llamas: {nombre}\nTu edad es: {edad}\nEres: {sexo}")
--- entradas-salidas/solicitar_vocal.py
+++ entradas-salidas/solicitar_vocal.py
@@ -1,8 +1,8 @@
 #solictar al usuario una vocal en minúscula
-vocal_minúscula = input("Introduce una vocal en minúscula")
+vocal_minúscula = input("Introduce una vocal en minúscula: ")
 
 # solicitar al usuario una vocal en mayúscula
-vocal_mayúscula = input("Introduce una vocal mayúscula")
+vocal_mayúscula = input("Introduce una vocal mayúscula: ")
 
 # invertir la vocal minúscula a mayúscula
 vocal_minúscula_convertida = vocal_minúscula.upper()
